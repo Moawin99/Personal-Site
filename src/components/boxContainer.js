4614c2c76af8712081669d8fd1ef63d2d861ad62
Mark Moawad <msmoawad99@gmail.com>
2020-12-17 20:43:36 -0500

    added the colored boxes

---
+++ src/components/boxContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import boxStyles from './boxContainer.module.css';
+
+const ColorBox = () => (
+    <div className={boxStyles.colorBox} />
+)
+
+export default ColorBox;
--- src/components/introPic.js
+++ src/components/introPic.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import introDiv from './introDivs.module.css';
 import mePic from '../images/BedroomPic.jpg';
+import ColorBox from './boxContainer';
 
 const IntroPic = () => (
     <div className={introDiv.introDiv}>
+         <ColorBox />
         <img className={introDiv.photo} src={mePic} />
     </div>
 )
