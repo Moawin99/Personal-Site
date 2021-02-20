24cb2df71dd4b8cfcc6f02bb04608eb755c3dbab
Mark Moawad <msmoawad99@gmail.com>
2021-02-19 21:45:05 -0500

    began working on about me box

--- src/components/aboutMeContainer.js
+++ src/components/aboutMeContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import aboutMeStyles from './aboutMeContainer.module.css';
-import layoutStyle from './layout.module.css';
 
 
 const AboutMe = (props) => ( 
     <div className={aboutMeStyles.aboutMeBox}>
-        <h2>Test</h2>
+        <div>
+            <h1>Hello!</h1>
+        </div>
     </div>
 )
 
