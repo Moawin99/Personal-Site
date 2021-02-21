aae7858b70f507d43bd8e1cc3d7f4a10bdf39d38
Mark Moawad <msmoawad99@gmail.com>
2021-02-20 22:09:17 -0500

    adding fonts to site

--- src/components/aboutMeContainer.js
+++ src/components/aboutMeContainer.js
@@ -4,8 +4,10 @@ import aboutMeStyles from './aboutMeContainer.module.css';
 
 const AboutMe = (props) => ( 
     <div className={aboutMeStyles.aboutMeBox}>
-        <div>
-            <h1>Hello!</h1>
+        <div className={aboutMeStyles.textContainer}>
+            <h1>Hello! I'm Mark, a thrid year Computer Science student at <span className={aboutMeStyles.sbu}>Stony Brook University</span>. 
+            I have a passion for teaching myself new things and am always eager to learn!.
+            Besides programming, I enjoy listening to music, as well as collecting records, Longboarding and playing basketball. </h1>
         </div>
     </div>
 )
