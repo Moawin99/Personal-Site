8d8cbf6cdd58143fc3532909651b724e73cefedb
Mark Moawad <msmoawad99@gmail.com>
2021-02-21 21:40:04 -0500

    started project box

--- src/components/aboutMeContainer.js
+++ src/components/aboutMeContainer.js
@@ -5,9 +5,10 @@ import aboutMeStyles from './aboutMeContainer.module.css';
 const AboutMe = (props) => ( 
     <div className={aboutMeStyles.aboutMeBox}>
         <div className={aboutMeStyles.textContainer}>
+            <h1 className={aboutMeStyles.aboutMeText}>About Me</h1>
             <h1>Hello! I'm Mark, a thrid year Computer Science student at <span className={aboutMeStyles.sbu}>Stony Brook University</span>. 
-            I have a passion for teaching myself new things and am always eager to learn!.
-            Besides programming, I enjoy listening to music, as well as collecting records, Longboarding and playing basketball. </h1>
+            I have a passion for teaching myself new things and am always eager to learn!
+            Besides programming, I enjoy listening to music, as well as collecting records, longboarding and playing basketball. </h1>
         </div>
     </div>
 )
