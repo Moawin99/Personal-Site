9ea7f8a10d7a094419a51e439171463104a3b802
Mark Moawad <msmoawad99@gmail.com>
2021-03-06 23:11:16 -0500

    began making project card

---
+++ src/components/projectCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import cardStyles from './projectCard.module.css';
+
+const ProjectCard = (props) => (
+        <div className={cardStyles.cardContainer}>
+            <h2 className={cardStyles.title}>{props.name}</h2>
+            <div className={cardStyles.bioContainer}>
+                 <p>{props.bio}</p>
+            </div>
+            <hr className={cardStyles.line} />
+            <div className={cardStyles.buttonContainer}>
+                <button><a href={props.gitUrl}>Github Url</a></button>
+                {props.isLive ? <button><a href={props.projectUrl}>Project Url</a></button>: null}
+            </div>
+        </div>
+)
+
+export default ProjectCard
--- src/components/projectContainer.js
+++ src/components/projectContainer.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ProjectCard from './projectCard';
 import boxStyles from './projectContainer.module.css';
 
 const ProjectBox = (props) => (
     <div className={boxStyles.projectBox}>
-        <h2>
-            Projects!
-        </h2>
+        <ProjectCard 
+        name="InternZip"
+        bio="Hackathon project that was awarded best domain in CEWIT 2021"
+        isLive={true}
+        />
     </div>
 )
 
