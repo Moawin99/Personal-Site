f05fd38f34ffe24032a67befa0bdfecb56c4993c
Mark Moawad <msmoawad99@gmail.com>
2021-03-09 09:40:25 -0500

    working on project cards

--- src/components/projectContainer.js
+++ src/components/projectContainer.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ProjectCard from './projectCard';
 import boxStyles from './projectContainer.module.css';
 
+const data = [
+    {
+        "name": "InternZip",
+        "github": "https://github.com/Moawin99/CEWIT-Hackathon-2021",
+        "bio": "Full stack Hackathon project that acts as a platfrom for interns to find housing for internships. Won best domain",
+        "link": "https://curious-setup-306205.df.r.appspot.com/",
+        "isLive": true
+    }
+];
+
+
 const ProjectBox = (props) => (
     <div className={boxStyles.projectBox}>
-        <ProjectCard 
-        name="InternZip"
-        bio="Hackathon project that was awarded best domain in CEWIT 2021"
-        isLive={true}
-        />
+        <h1 className={boxStyles.text}>Projects</h1>
+        <div className={boxStyles.gridContainer}>
+
+        </div>
     </div>
 )
 
