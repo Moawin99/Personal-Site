dedd77c555c487405d99e531a0b1846c451a69f7
Mark Moawad <msmoawad99@gmail.com>
2021-03-09 22:57:35 -0500

    finished mapping project cards

--- src/components/projectContainer.js
+++ src/components/projectContainer.js
@@ -9,6 +9,36 @@ const data = [
         "bio": "Full stack Hackathon project that acts as a platfrom for interns to find housing for internships. Won best domain",
         "link": "https://curious-setup-306205.df.r.appspot.com/",
         "isLive": true
+    },
+    {
+        "name": "Discord-Bot",
+        "github": "https://github.com/Moawin99/discord-bot",
+        "bio": "Discord bot written in python. Includes games that can be played in a server, as well as data fetching from multiple api's to display motivational quotes",
+        "isLive": false
+    },
+    {
+        "name": "Writtery",
+        "github": "https://github.com/Moawin99/Journal",
+        "bio": "Full stack Nodejs and React project that serves as a journnal that plays music according to your mood. The utalizes the Spotify Api and filters songs according to mood",
+        "isLive": false
+    },
+    {
+        "name": "Ghost Kitchen",
+        "github": "https://github.com/Moawin99/GhostKitchen",
+        "bio": "Full stack application built with Spring boot and React. Templete kithcen app, users can order food and owners can register restaurants and create menu items for purchase",
+        "isLive": false
+    },
+    {
+        "name": "Prodict",
+        "github": "https://github.com/Moawin99/CEWIT-Hackthon-Project",
+        "bio": "Machine learning model that predictits Nba mvp. Built in python. Also won CEWIT 2020 best use of google cloud",
+        "isLive": false
+    },
+    {
+        "name": "Personal Site",
+        "github": "https://github.com/Moawin99/Personal-Site",
+        "bio": "The site you're on now! Built in Gatsby js",
+        "isLive": false
     }
 ];
 
@@ -17,7 +47,15 @@ const ProjectBox = (props) => (
     <div className={boxStyles.projectBox}>
         <h1 className={boxStyles.text}>Projects</h1>
         <div className={boxStyles.gridContainer}>
-
+            {data.map((project) => (
+                <ProjectCard
+                name={project.name}
+                bio={project.bio}
+                gitUrl={project.github}
+                isLive={project.isLive}
+                projectUrl={project.link}
+                />
+            ))}
         </div>
     </div>
 )
