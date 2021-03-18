17a1f75e31f90a51a01516994468a650aa74acf4
Mark Moawad <msmoawad99@gmail.com>
2021-03-18 15:27:59 -0400

    added footer component and global css

--- src/components/resumeContainer.js
+++ src/components/resumeContainer.js
@@ -4,7 +4,7 @@ import boxStyles from './resumeContainer.module.css';
 const ResumeBox = (props) => (
         <div className={boxStyles.container}>
             <h1>My Resume</h1>
-            <iframe src="https://drive.google.com/file/d/16UirjUjx0iPaKGs9F4k2jR2N3Ntq4Hfn/preview" width="1000px" height="1000px"></iframe>
+            <iframe src="https://drive.google.com/file/d/1Er61HkyYfVd7_frdMv7ig2t4a32YxD4v/preview" width="1000" height="1000"></iframe>
         </div>
 )
 
