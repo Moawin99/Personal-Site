d8ce1dc1baf821f4ce0ccd1730be6fc1fb228b94
Mark Moawad <msmoawad99@gmail.com>
2021-03-30 00:31:53 -0400

    final touches

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,10 @@
-import React from "react"
+import React, { useEffect } from "react"
 import layoutSyles from './layout.module.css';
 
 const Layout = ({ children }) => {
+  useEffect(() => {
+    document.title = "Mark Moawad"
+  }, [])
   return (
     <div className={layoutSyles.layoutDiv}>
       {children}
