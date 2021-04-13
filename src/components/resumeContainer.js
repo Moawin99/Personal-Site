428118687565bad70d73a75ae8c5ff7efd621633
Mark Moawad <msmoawad99@gmail.com>
2021-04-12 23:01:44 -0400

    updated Resume

--- src/components/resumeContainer.js
+++ src/components/resumeContainer.js
@@ -4,7 +4,7 @@ import boxStyles from './resumeContainer.module.css';
 const ResumeBox = (props) => (
         <div id="resume" className={boxStyles.container}>
             <h1 className={boxStyles.resumeTitle}>My Resume</h1>
-            <object className={boxStyles.resume} type="application/pdf" data="https://drive.google.com/file/d/1Er61HkyYfVd7_frdMv7ig2t4a32YxD4v/preview" width="1000px" height="1000px" />
+            <iframe className={boxStyles.resume} src="https://drive.google.com/file/d/1QTzYJ1Qq-_kR4Nd88GCZxGIaonyqo-LP/preview"></iframe>
         </div>
 )
 
