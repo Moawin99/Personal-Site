7d34e109e0563b605d5f55bdacb239bba96effd3
Mark Moawad <msmoawad99@gmail.com>
2021-08-22 21:30:54 -0400

    senoir status baby

--- src/components/aboutMeContainer.js
+++ src/components/aboutMeContainer.js
@@ -6,7 +6,7 @@ const AboutMe = (props) => (
     <div id="aboutMe" className={aboutMeStyles.aboutMeBox}>
         <div className={aboutMeStyles.textContainer}>
             <h1 className={aboutMeStyles.aboutMeText}>About Me</h1>
-            <p className={aboutMeStyles.text}>Hello! I'm Mark, a third year Computer Science student at <span className={aboutMeStyles.sbu}>Stony Brook University</span>. 
+            <p className={aboutMeStyles.text}>Hello! I'm Mark, a senoir Computer Science student at <span className={aboutMeStyles.sbu}>Stony Brook University</span>. 
             I have a passion for teaching myself new things and all things programming!
            I also enjoy listening to music, as well as collecting records, longboarding and playing basketball. </p>
         </div>
