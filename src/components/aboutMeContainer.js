421f3680d64a25cf4aa7f11a4fdcf605b06fea4f
Mark Moawad <msmoawad99@gmail.com>
2021-08-26 22:33:39 -0400

    about me changes

--- src/components/aboutMeContainer.js
+++ src/components/aboutMeContainer.js
@@ -8,7 +8,8 @@ const AboutMe = (props) => (
             <h1 className={aboutMeStyles.aboutMeText}>About Me</h1>
             <p className={aboutMeStyles.text}>Hello! I'm Mark, a senoir Computer Science student at <span className={aboutMeStyles.sbu}>Stony Brook University</span>. 
             I have a passion for teaching myself new things and all things programming!
-           I also enjoy listening to music, as well as collecting records, longboarding and playing basketball. </p>
+           In my free time I enjoy photography, reading, longbaording, and playing basketball. Anytime I encounter a new intrest it I get addicted to learning more about it.
+           I also love listening to music and even collect vinyl records!</p>
         </div>
     </div>
 )
